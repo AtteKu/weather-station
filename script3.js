e3d7d321a54dc14aa242f91f6fd6cbab96e7eea6
Atte Kukkonen <kukkonen16@hotmail.com>
2025-04-15 16:07:45 +0300

    Phase 1

---
+++ script3.js
@@ -0,0 +1,82 @@
+async function fetchWeatherData(latitude, longitude) {
+    const params = new URLSearchParams({
+      latitude: latitude.toString(),
+      longitude: longitude.toString(),
+      hourly: "temperature_2m,rain,windspeed_10m"
+    });
+  
+    const url = `https://api.open-meteo.com/v1/forecast?${params.toString()}`;
+  
+    try {
+      // Lähetetään GET-pyyntö Open-Meteo API:lle
+      const response = await fetch(url);
+  
+      // Tarkistetaan, että pyyntö onnistui
+      if (!response.ok) {
+        throw new Error('API-call failed');
+      }
+  
+      // JSON-vastaus
+      const data = await response.json();
+  
+      // Tarkistetaan, että data löytyy
+      if (!data || !data.hourly || !data.hourly.temperature_2m || !data.hourly.time || !data.hourly.rain || !data.hourly.windspeed_10m) {
+        throw new Error('Missing Info');
+      }
+  
+      // Muodostetaan säätiedot
+      const weatherData = {
+        hourly: {
+          time: data.hourly.time.map((t) => new Date(t)),
+          temperature2m: data.hourly.temperature_2m,
+          rain: data.hourly.rain,
+          windspeed: data.hourly.windspeed_10m,
+        },
+      };
+  
+      const tbody = document.querySelector("#data-table3 tbody");
+      const len = weatherData.hourly.time.length;
+      
+      const now = new Date();
+      const pastData = weatherData.hourly.time.filter(time => time <=now);
+
+      const start = Math.max(0, pastData.length - 20);
+  
+      
+      for (let i = start; i < len; i++) {
+        const dateString = pastData[i].toLocaleString("fi-Fi", {
+          weekday: "long",
+          year: "numeric",
+          month: "2-digit",
+          day: "2-digit"
+        });
+
+        const timeString = pastData[i].toLocaleString("fi-Fi", {
+            hour: "2-digit",
+            minute: "2-digit"
+          });
+  
+        const windTemp = weatherData.hourly.windspeed[i].toFixed(1);
+
+        const windMperS = windTemp / 3.6;
+        
+        const wind = Math.round(windMperS)
+  
+        const rowWind = document.createElement("tr");
+  
+        rowWind.innerHTML = `
+        <td>${dateString}</td>
+        <td>${timeString}</td>
+        <td>${wind} m/s</td>
+      `;
+      
+      tbody.appendChild(rowWind);
+  
+      }
+  
+    } catch (error) {
+      console.error("Error:", error);
+    }
+  }
+  
+  fetchWeatherData(61.4991, 23.7871);
